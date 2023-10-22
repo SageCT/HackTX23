8130b42edd6694c1199ee490e1f0e7d8f38e59fd
ujwal2003 <joshiujwal2003@gmail.com>
2023-10-21 20:59:01 -0500

    implemented api call

---
+++ hackTX23/src/server/bls-data/seriesID.ts
@@ -0,0 +1,44 @@
+export const nameUS_ALL = {
+    "All_Occupations": "OEUN000000000000015000004",
+    "Info_Sec": "OEUN000000000000015121204",
+    "Comp_Sci_Analyst": "OEUN000000000000015121104",
+    "CIS": "OEUN000000000000015122104",
+    "Network_Support": "OEUN000000000000015123104",
+    "Comp_User_Supp": "OEUN000000000000015123204",
+    "Database_Architects": "OEUN000000000000015124304",
+    "Network_Architects": "OEUN000000000000015124404",
+    "Web_Dev": "OEUN000000000000015125404",
+    "Web_Designer": "OEUN000000000000015125504",
+    "Software_Dev": "OEUN000000000000015125204",
+    "Math": "OEUN000000000000015202104",
+    "Stats": "OEUN000000000000015204104",
+    "Data_Sci": "OEUN000000000000015205104"
+}
+
+export const dummy = {
+    "status": "REQUEST_SUCCEEDED",
+    "responseTime": 179,
+    "message": [
+        "No Data Available for Series OEUN000000000000015125204 Year: 2020",
+        "No Data Available for Series OEUN000000000000015125204 Year: 2021"
+    ],
+    "Results": {
+        "series": [
+            {
+                "seriesID": "OEUN000000000000015125204",
+                "data": [
+                    {
+                        "year": "2022",
+                        "period": "A01",
+                        "periodName": "Annual",
+                        "latest": "true",
+                        "value": "132930",
+                        "footnotes": [
+                            {}
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+}
--- hackTX23/src/server/main.ts
+++ hackTX23/src/server/main.ts
@@ -1,12 +1,15 @@
 import express from "express";
-import ViteExpress from "vite-express";
 
 const app = express();
 
-app.get("/hello", (_, res) => {
-  res.send("Hello Vite + React + TypeScript!");
+app.get("/", (_, res) => {
+  res.send("Hello, World");
 });
 
-ViteExpress.listen(app, 3000, () =>
+app.get("/oe-codes", (req, res) => {
+  res.send("test");
+});
+
+app.listen(3000, () => {
   console.log("Server is listening on port 3000...")
-);
+});
