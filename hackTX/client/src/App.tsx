51211b16ed12009f32d46e716b205e37fd21ca97
SageCT <99381480+SageCT@users.noreply.github.com>
2023-10-21 22:44:00 -0500

    feat: frontend moved to client file

---
+++ hackTX/client/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
+
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ hackTX/client/src/pages/LandingPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Landingpage = () => {
+  return (
+    <>
+      <div className="">
+        <div className=""></div>
+      </div>
+    </>
+  );
+};
+
+export default Landingpage;
