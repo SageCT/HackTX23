cada6a51048b7d63eff9591b9ff6b9ea070f5de3
KevinZheng14815 <91815800+KevinZheng14815@users.noreply.github.com>
2023-10-21 23:36:02 -0500

    removed vite

--- hackTX23/src/server/main.ts
+++ hackTX23/src/server/main.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import ViteExpress from "vite-express";
+
 
 const app = express();
 const bcrypt = require('bcrypt');
@@ -71,6 +71,6 @@ app.get("/hello", (_, res) => {
   res.send("Hello Vite + React + TypeScript!");
 });
 
-ViteExpress.listen(app, 3000, () => 
+app.listen(3000, () => 
   console.log("Server is listening on port 3000...")
 );
