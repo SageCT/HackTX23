6c98f25064f5d4dd38330f7cf50af2e4a18006ac
SageCT <99381480+SageCT@users.noreply.github.com>
2023-10-22 06:40:28 -0500

    Finalized quiz pages

--- hackTX/client/src/pages/QuestionPage.tsx
+++ hackTX/client/src/pages/QuestionPage.tsx
@@ -1,18 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import Footer from "../components/Footer";
-
-const affinity = [["web_dev", 0],["info_sec", 0],["database_arc",0]];
-
-
-
-function QuestionsResponse() {
-  const handleClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-    e.preventDefault();
-    
-}
-
+import { useNavigate } from "react-router-dom";
 
 const QuestionPage = () => {
+  const navigate = useNavigate();
+  const [results, setResults] = useState(false);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [affinity, setAffinity] = useState({
+    web_dev: 0,
+    info_sec: 0,
+    database_arc: 0,
+  });
+  const questions = [
+    "I enjoy activities like art, writing, or other creative outlets.",
+    "I prefer working alone on important projects.",
+    "I want to pursue post-graduate education.",
+    "I have a keen eye for details or mistakes.",
+    "I find complex systems interesting.",
+    "I enjoy making sure everyone is able to stay safe and secure",
+  ];
+
+  const handleResponse = (response: number) => {
+    const affinityType =
+      currentQuestion % 3 === 0
+        ? "web_dev"
+        : currentQuestion % 3 === 1
+        ? "info_sec"
+        : "database_arc";
+    const newAffinity = { ...affinity };
+    if (response === 1) {
+      newAffinity[affinityType] += 1;
+    } else if (response === 5) {
+      newAffinity[affinityType] -= 1;
+    }
+    setAffinity(newAffinity);
+    setCurrentQuestion(currentQuestion + 1);
+    if (currentQuestion === questions.length - 1) {
+      navigate("/FinalPage");
+    }
+  };
+
   return (
     <>
       <section
@@ -20,7 +47,7 @@ const QuestionPage = () => {
         className="mx-auto my-16 p-4 border-4 border-black rounded-lg max-w-3xl text-center"
       >
         <div id="Question" className="mb-4 text-xl font-semibold p-4">
-          I enjoy activities like art, writing, or other creative outlets.
+          {questions[currentQuestion]}
         </div>
 
         <div className="flex flex-col gap-3">
@@ -28,45 +55,41 @@ const QuestionPage = () => {
             <a
               href="#"
               className="block p-2 text-left hover:bg-gray-200 hover:border-gray-400"
+              onClick={() => handleResponse(1)}
             >
               Strongly agree
             </a>
-          </div>
-          <div className="border-2 border-black rounded-xl p-2">
             <a
               href="#"
               className="block p-2 text-left hover:bg-gray-200 hover:border-gray-400"
+              onClick={() => handleResponse(2)}
             >
               Agree
             </a>
-          </div>
-          <div className="border-2 border-black rounded-xl p-2">
             <a
               href="#"
               className="block p-2 text-left hover:bg-gray-200 hover:border-gray-400"
+              onClick={() => handleResponse(3)}
             >
-              Neither agree nor disagree
+              Neutral
             </a>
-          </div>
-          <div className="border-2 border-black rounded-xl p-2">
             <a
               href="#"
               className="block p-2 text-left hover:bg-gray-200 hover:border-gray-400"
+              onClick={() => handleResponse(4)}
             >
               Disagree
             </a>
-          </div>
-          <div className="border-2 border-black rounded-xl p-2">
             <a
               href="#"
               className="block p-2 text-left hover:bg-gray-200 hover:border-gray-400"
+              onClick={() => handleResponse(5)}
             >
               Strongly disagree
             </a>
           </div>
         </div>
       </section>
-
       <Footer />
     </>
   );
