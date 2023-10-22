e081c920a881b3acfbd4117f197d2b37cc8c3226
SageCT <99381480+SageCT@users.noreply.github.com>
2023-10-22 03:20:49 -0500

    breaking: footer formatting needs fix

--- hackTX/client/src/components/Footer.tsx
+++ hackTX/client/src/components/Footer.tsx
@@ -1,25 +1,44 @@
 import React from "react";
 
+const GITHUBS = {
+  SAGE: "https://github.com/SageCT",
+  ALTON: "https://github.com/SageCT",
+  UJWAL: "https://github.com/SageCT",
+  KEVIN: "https://github.com/SageCT",
+};
+
 const Footer = () => {
   return (
     <>
-      <div className="fixed flex flex-row justify-center items-center bottom-0 h-12 w-screen bg-navgray">
-        <p className="text-sm">{"Created for HackTX2023 | Created by "}</p>
-        <a href="#">
-          <p>@altonph </p>
-        </a>
-        <p> </p>
-        <a href="https://github.com/SageCT">
-          <p>@SageCT</p>
-        </a>
-        <p> </p>
-        <a href="#">
-          <p>@altonph </p>
-        </a>
-        <p> </p>
-        <a href="#">
-          <p>@altonph</p>
-        </a>
+      <div className="flex flex-row justify-center items-center bottom-0 h-12 w-screen text-black bg-navgray">
+        <p className="text-sm">{"Created for HackTX 2023 | "}</p>
+
+        <p>
+          {" "}
+          <a href={GITHUBS.ALTON}>
+            <u>@altonp</u>
+          </a>{" "}
+        </p>
+
+        <p>
+          {" "}
+          <a href={GITHUBS.SAGE}>
+            <u>@SageCT</u>
+          </a>{" "}
+        </p>
+
+        <p>
+          <a href={GITHUBS.UJWAL}>
+            <u> @UJWAL </u>
+          </a>
+        </p>
+
+        <p>
+          {" "}
+          <a href={GITHUBS.KEVIN}>
+            <u>@KEVIN</u>
+          </a>{" "}
+        </p>
       </div>
     </>
   );
--- hackTX/client/src/pages/LandingPage.tsx
+++ hackTX/client/src/pages/LandingPage.tsx
@@ -1,20 +1,22 @@
 import React from "react";
-import NavBar from "../components/NavBar"
-import Footer from "../components/Footer"
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 import ColoredShape from "../components/ColoredShape";
 
 const Landingpage = () => {
   return (
     <>
-      <NavBar/>
-      {/* Title Test */}
-      <div className="">
-        <h1 className="font-opensans">HELLO THIS IS A TESTERINO</h1>
-      </div>
+      <NavBar />
+      <div className="bg-white h-screen">
+        {/* Title Test */}
+        <div className="">
+          <h1 className="font-opensans">HELLO THIS IS A TESTERINO</h1>
+        </div>
 
-      {/* Component Test */}
-      <ColoredShape type="circle" color="red" />
-      <Footer/>
+        {/* Component Test */}
+        <ColoredShape type="circle" color="red" />
+      </div>
+      <Footer />
     </>
   );
 };
