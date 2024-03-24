c20c665d66243f5184f09cd9380c9913b9ab3d42
SageCT <99381480+SageCT@users.noreply.github.com>
2024-03-24 14:33:47 -0500

    Changed package managers from npm to pnpm

--- server/src/server/bls-data/occupations.ts
+++ server/src/server/bls-data/occupations.ts
@@ -1,13 +1,12 @@
-const fetch = require('node-fetch');
-
+import fetch from 'node-fetch';
 export async function getBLSAPICallResult(seriesID: string) {
-    const url: string = `https://api.bls.gov/publicAPI/v2/timeseries/data/${seriesID}`;
-    try {
-        const response = await fetch(url);
-        const jsonData = await response.json();
+  const url: string = `https://api.bls.gov/publicAPI/v2/timeseries/data/${seriesID}`;
+  try {
+    const response = await fetch(url);
+    const jsonData = await response.json();
 
-        return jsonData;
-    } catch (error) {
-        console.error(error);
-    }
+    return jsonData;
+  } catch (error) {
+    console.error(error);
+  }
 }
